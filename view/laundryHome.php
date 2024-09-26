1777624d4dbda142804df5543996f6156671dc29
soumikhasan22 <soumikhasan000@gmail.com>
2024-09-26 13:27:42 +0600

    Create laundryHome.php

---
+++ view/laundryHome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <link rel="stylesheet" href="../css/style3.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Laundry Service</h1>
+        <div class="button-container">
+            <a href="requestForm.php" class="button">Request Laundry</a>
+            <a href="dashboard.php" class="button">View Dashboard</a>
+            <a href="managementdashboard.php" class="button">Management Dashboard</a>
+           
+        </div>
+    </div>
+</body>
+</html>
